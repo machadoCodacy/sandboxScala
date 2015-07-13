adfe09b9b155765ea7474804c7581c4a94a3f530
machado <machado@codacy.com>
2015-07-13 16:29:57 +0100

    first commit

---
+++ src/Program.scala
@@ -0,0 +1,15 @@
+/**
+ * Created by maxadoj on 13-07-2015.
+ */
+object Program {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+
+    //var? no no no!
+    var variavel1 = 42;
+
+    println(variavel1)
+
+    println("Bye!")
+  }
+}
