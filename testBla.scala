f89005679d48c9ef4c728f45d17b811c27f74759
Joao Machado <machado@codacy.com>
2016-02-17 15:01:36 +0000

    create a test

---
+++ testBla.scala
@@ -0,0 +1,17 @@
+import scala.util.{Failure, Success, Try}
+
+Try(1/ 0) match {
+  case Success(content) => "yey"
+  case Failure(fail) => "nop"
+}
+
+List(1,2,3).collectFirst {
+  case bla if bla > 2 => "yey"
+  case blaa if blaa > 1 => "nop"
+}
+
+//BUM!!!
+List(1,2,3).map {
+  case bla if bla > 2 => "yey"
+  case blaa if blaa > 1 => "nop"
+}
